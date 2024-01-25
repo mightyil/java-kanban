8343a7ed0c4be638b391e4f21e846a7eccedbf9c
mightyil <mightyill@yandex.ru>
2024-01-25 15:11:57 +0700

    some tests added in TaskManagerTest

--- test/TaskManagerTest.java
+++ test/TaskManagerTest.java
@@ -2,12 +2,73 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TaskManagerTest {
 
-    @BeforeAll
-    static void beforeAll() {
+    TaskManager taskManager;
 
+    @BeforeEach
+    void beforeEach() {
+        taskManager = Managers.getDefault();
+    }
+
+    @Test
+    void createTask() {
+        Task task = new Task("Test createTask", "Test createTask description", TaskStatus.NEW);
+        final int taskId = taskManager.createTask(task);
+
+        final Task savedTask = taskManager.getTaskById(taskId);
+
+        assertNotNull(savedTask, "Задача не найдена.");
+        assertEquals(task, savedTask, "Задачи не совпадают.");
+
+        final List<Task> tasks = taskManager.getTasks();
+
+        assertNotNull(tasks, "Задачи не возвращаются.");
+        assertEquals(1, tasks.size(), "Неверное количество задач.");
+        assertEquals(task, tasks.get(0), "Задачи не совпадают.");
+    }
+
+    @Test
+    void createSubTask() {
+        Epic epic = new Epic("task", "description", 0);
+        SubTask subTask = new SubTask(
+                "Test createSubTask",
+                "Test createSubTask description",
+                TaskStatus.NEW,
+                epic
+        );
+        final int subTaskId = taskManager.createSubTask(subTask);
+
+        final SubTask savedSubTask = taskManager.getSubTaskById(subTaskId);
+
+        assertNotNull(savedSubTask, "Задача не найдена.");
+        assertEquals(subTask, savedSubTask, "Задачи не совпадают.");
+
+        final List<SubTask> subTasks = taskManager.getSubTasks();
+
+        assertNotNull(subTasks, "Задачи не возвращаются.");
+        assertEquals(1, subTasks.size(), "Неверное количество задач.");
+        assertEquals(subTask, subTasks.get(0), "Задачи не совпадают.");
+    }
+
+    @Test
+    void createEpic () {
+        Epic epic = new Epic("Test createEpic", "Test createEpic description");
+        final int epicId = taskManager.createEpic(epic);
+
+        final Epic savedEpic = taskManager.getEpicById(epicId);
+
+        assertNotNull(savedEpic, "Задача не найдена.");
+        assertEquals(epic, savedEpic, "Задачи не совпадают.");
+
+        final List<Epic> tasks = taskManager.getEpics();
+
+        assertNotNull(tasks, "Задачи не возвращаются.");
+        assertEquals(1, tasks.size(), "Неверное количество задач.");
+        assertEquals(epic, tasks.get(0), "Задачи не совпадают.");
     }
 }
